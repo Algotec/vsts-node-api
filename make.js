ba6215c91542f43c7e5cac82a7cf8da88dc5c8bb
nadavsinai <ns@nadavsinai.com>
2018-08-20 00:10:56 +0300

    build that works well for lerna environment

--- make.js
+++ make.js
@@ -2,62 +2,93 @@ require('shelljs/make');
 var path = require('path');
 var fs = require('fs');
 
-var rp = function(relPath) {
-    return path.join(__dirname, relPath);
+var rp = function (relPath) {
+	return path.join(__dirname, relPath);
 }
 
 var buildPath = path.join(__dirname, '_build');
 var testPath = path.join(__dirname, '_test');
 
-var run = function(cl) {
-    console.log('> ' + cl);
-    var rc = exec(cl).code;
-    if (rc !== 0) {
-        echo('Exec failed with rc ' + rc);
-        exit(rc);
-    }
+var run = function (cl) {
+	console.log('> ' + cl);
+	var rc = exec(cl).code;
+	if (rc !== 0) {
+		echo('Exec failed with rc ' + rc);
+		exit(rc);
+	}
+}
+function renamePackage(pjsonPath = './_build/package.json') {
+	const pjson = require(pjsonPath);
+	pjson.name = pjson.name.replace('-sources', '');
+	fs.writeFileSync(pjsonPath, JSON.stringify(pjson));
 }
 
-target.clean = function() {
-    rm('-Rf', buildPath);
-    rm('-Rf', testPath);
+target.clean = function () {
+	rm('-Rf', buildPath);
+	rm('-Rf', testPath);
 };
 
-target.build = function() {
-    target.clean();
+target.build = function () {
+	target.clean();
 
-    run('tsc --outDir ' + buildPath);
+	run('tsc --outDir ' + buildPath);
 
-    cp('-Rf', rp('api/opensource'), buildPath);
-    
-    cp(rp('LICENSE'), buildPath);
-    cp(rp('package.json'), buildPath);
-    cp(rp('package-lock.json'), buildPath);
-    cp(rp('ThirdPartyNotice.txt'), buildPath);
-    cp(rp('README.md'), buildPath);
-    
-    // just a bootstrap file to avoid /// in final js and .d.ts file
-    rm(path.join(buildPath, 'index.*'));
+	cp('-Rf', rp('api/opensource'), buildPath);
+
+	cp(rp('LICENSE'), buildPath);
+	cp(rp('package.json'), buildPath);
+	cp(rp('package-lock.json'), buildPath);
+	cp(rp('ThirdPartyNotice.txt'), buildPath);
+	cp(rp('README.md'), buildPath);
+	sourcesToBuild();
+	// just a bootstrap file to avoid /// in final js and .d.ts file
+	rm(path.join(buildPath, 'index.*'));
+}
+function renamePackage(pjson) {
+	pjson.name = pjson.name.replace('-sources', '');
+	return pjson;
+}
+
+function write(path) {
+	return (content) => {
+		fs.writeFileSync(path, JSON.stringify(content), null, 2);
+	}
+}
+
+function sourcesToBuild(buildPath = '_build') {
+	const pjsonPath = './' + buildPath + '/package.json';
+	const pljsonPath = './' + buildPath + '/package-lock.json';
+
+	[pjsonPath, pljsonPath]
+		.map((path) => ({ path, content: require(path) }))
+		.map(({ path, content }) => ({ path, content: renamePackage(content) }))
+		.map(({ path, content }) => ({ path, content: removeScripts(content) }))
+		.map(({ path, content }) => write(path)(content));
+}
+
+function removeScripts(pjson) {
+	delete pjson.scripts;
+	return pjson;
 }
 
 // test is just building samples
-target.test = function() {
-    target.build();
+target.test = function () {
+	target.build();
 
-    var modPath = path.join(__dirname, 'samples', 'node_modules');
-    rm('-Rf', modPath);
-    mkdir('-p', modPath);
-    pushd('samples');
-    run('npm install ../_build');
-    popd();
-    run('tsc -p samples');
+	var modPath = path.join(__dirname, 'samples', 'node_modules');
+	rm('-Rf', modPath);
+	mkdir('-p', modPath);
+	pushd('samples');
+	run('npm install ../_build');
+	popd();
+	run('tsc -p samples');
 }
 
-target.samples = function() {
-    target.test();
+target.samples = function () {
+	target.test();
 
-    pushd('samples');
-    run('node run.js');
-    popd();
-    console.log('done');
+	pushd('samples');
+	run('node run.js');
+	popd();
+	console.log('done');
 }
